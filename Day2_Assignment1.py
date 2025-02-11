cc3741c76e637631ebd724e95e4dfa4bbe9780ff
prajyot53 <114487734+prajyot53@users.noreply.github.com>
2025-02-11 13:31:05 +0530

    Add files via upload

---
+++ Day2_Assignment1.py
@@ -0,0 +1,16 @@
+Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # Take input from the user
+... num1 = float(input("Enter first number: "))
+Enter first number: 3
+>>> num2 = float(input("Enter second number: "))
+... 
+Enter second number: 5
+>>> # Calculate sum and multiplication
+... sum_result = num1 + num2
+>>> multiplication_result = num1 * num2
+>>> # Display the results
+... print(f"Sum: {sum_result}")
+Sum: 8.0
+>>> print(f"Multiplication: {multiplication_result}")
+Multiplication: 15.0
---
+++ Day2_Assignment2.py
@@ -0,0 +1,10 @@
+Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # Declare two variables
+... a = 10
+>>> b = 20
+>>> # Find the largest using a ternary operator
+... largest = a if a > b else b
+>>> # Print the largest variable
+... print(f"The largest number is: {largest}")
+The largest number is: 20
